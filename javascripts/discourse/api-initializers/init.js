b3a2074d6c09108ab4bec4b52df63e60445d3751
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-04 21:33:03 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -2,16 +2,5 @@ import { apiInitializer } from "discourse/lib/api";
 import AddCodeblockButton from "../components/add-codeblock-button";
 
 export default apiInitializer((api) => {
-  api.registerValueTransformer(
-    "post-menu-buttons",
-    ({
-      value: dag,
-      context: { lastHiddenButtonKey, secondLastHiddenButtonKey },
-    }) => {
-      dag.add("show-raw", AddCodeblockButton, {
-        before: lastHiddenButtonKey,
-        after: secondLastHiddenButtonKey,
-      });
-    }
-  );
+  api.renderInOutlet("quote-share-buttons-before", AddCodeblockButton);
 });
